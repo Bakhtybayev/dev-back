14a0e7b634d14651bf249ee1f257020c9a05114d
Yeldar Bakhtybayev <yeldar.bakhtybayev@ntpayments.com>
2023-10-13 20:41:07 +0600

    cp

---
+++ src/main/java/com/dev/devdemo/auth/AuthenticationService.java
@@ -0,0 +1,36 @@
+package com.dev.devdemo.auth;
+
+import com.dev.devdemo.config.JwtService;
+import com.dev.devdemo.user.Role;
+import com.dev.devdemo.user.User;
+import com.dev.devdemo.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public AuthenticationResponse Register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+    public AuthenticationResponse Authenticate(AuthenticationRequest request) {
+        return null;
+    }
+
+}
--- src/main/java/com/dev/devdemo/user/Role.java
+++ src/main/java/com/dev/devdemo/user/Role.java
@@ -1,7 +1,6 @@
 package com.dev.devdemo.user;
 
 public enum Role {
-
     USER,
     ADMIN
 }
